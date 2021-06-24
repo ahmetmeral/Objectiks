f6f603bb95e2c4df69024ada3027e9d851b572b5
Ahmet MERAL <ahmetm@gmail.com>
2021-06-24 16:54:35 +0300

    1.0.3

--- Code/Core/Objectiks/Caching/Serializer/DocumentBsonSerializer.cs
+++ Code/Core/Objectiks/Caching/Serializer/DocumentBsonSerializer.cs
@@ -32,7 +32,7 @@ namespace Objectiks.Caching.Serializer
                 using (BsonDataWriter writer = new BsonDataWriter(ms))
                 {
                     JsonSerializer serializer = new JsonSerializer();
-                    serializer.Serialize(writer, item);
+                    serializer.Serialize(writer, item, item.GetType());
 
                     return ms.ToArray();
                 }
